29e1c0858154f8598311c7cfde12b415d89a7653
rday45 <rahularvinddayal@gmail.com>
2024-07-25 19:42:23 +1000

    added new cli command to drop all tables in the database

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -5,6 +5,11 @@ from models.user import User
 db_commands = Blueprint("db", __name__)
 
 @db_commands.cli.command("create")
-def create_tables():
+def create_all_tables():
     db.create_all()
-    print("Tables have been created")
+    print("All tables have been created")
+
+@db_commands.cli.command("drop")
+def drop_all_tables():
+    db.drop_all()
+    print("All tables have been dropped")
