143a2a31b8cb9a0efdc0ca45630fc70b81b16b43
rday45 <rahularvinddayal@gmail.com>
2024-07-25 19:38:21 +1000

    updated error relating to create command  in the cli controller

--- controllers/cli_controller.py
+++ controllers/cli_controller.py
@@ -4,7 +4,7 @@ from models.user import User
 
 db_commands = Blueprint("db", __name__)
 
-db_commands.cli.command("create")
+@db_commands.cli.command("create")
 def create_tables():
     db.create_all()
     print("Tables have been created")
